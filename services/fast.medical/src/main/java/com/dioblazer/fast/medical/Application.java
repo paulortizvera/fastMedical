435ef920c9f1b9cba6cf5b157f004088983a0342
Pa <paulortizvera@gmail.com>
2023-10-10 18:26:17 -0500

    Proyecto SpringBoot inicial

---
+++ services/fast.medical/src/main/java/com/dioblazer/fast/medical/Application.java
@@ -0,0 +1,13 @@
+package com.dioblazer.fast.medical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
